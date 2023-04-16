847b62735fc60f1dd00f5e7731ffe8200598bdaf
varun singh <singh.varun119@gmail.com>
2023-04-16 15:03:00 +0530

    added test cases

--- src/test.java
+++ src/test.java
@@ -13,4 +13,33 @@ public class test {
         assertEquals(100, account.getBalance(), 0);
 
     }
+
+    @Test
+    public void testWithdraw() throws Exception {
+        BankAccount account = new BankAccount("checking", new Customer("John"));
+        account.deposit(100);
+        account.withdraw(50);
+        assertEquals(50, account.getBalance(), 0);
+
+    }
+
+    @Test
+    public void testAddMoneyToStock() throws Exception {
+        BankAccount account = new BankAccount("checking", new Customer("John"));
+        account.deposit(100);
+        account.addMoneyToStock(50, "checking");
+        assertEquals(50, account.getBalance(), 0);
+
+    }
+
+    @Test
+    public void testBankAccount() throws Exception {
+        BankAccount account = new BankAccount("checking", new Customer("John"));
+        account.deposit(100);
+        account.withdraw(100);
+
+        assertEquals(0, account.getBalance(), 0);
+
+    }
+
 }
